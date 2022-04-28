8ddb04eb8e90decc45bdce1383a1bb287dd6a9f2
victor bobkov <victorbobkov@gmail.com>
2022-04-28 05:30:50 +0400

    Add form validation

--- script.js
+++ script.js
@@ -15,3 +15,35 @@ modalClose.addEventListener('click', () => modal.classList.remove('show-modal'))
 window.addEventListener('click', (e) => {
    e.target === modal ? modal.classList.remove('show-modal') : false
 })
+
+// Validate Form
+const validate = (nameValue, urlValue) => {
+   const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g
+   const regEx = new RegExp(expression)
+   if (!nameValue || !urlValue) {
+      alert('Please submit values for both fields.')
+      return false
+   }
+   if (!urlValue.match(regEx)) {
+      alert('Please provide a valid web address')
+      return false
+   }
+   // Valid
+   return true
+}
+
+// Handle Data from Form
+const storeBookmark = (e) => {
+   e.preventDefault()
+   const nameValue = websiteNameEl.value
+   let urlValue = websiteUrlEl.value
+   if (!urlValue.includes('http://', 'https://')) {
+      urlValue = `https://${urlValue}`
+   }
+   if (!validate(nameValue, urlValue)) {
+      return false
+   }
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark)
