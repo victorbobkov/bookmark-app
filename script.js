4b65e5a91eadae209a1b50741eeb312d5eb0ba82
victor bobkov <victorbobkov@gmail.com>
2022-04-28 05:10:28 +0400

    Add modal event listeners

---
+++ script.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById('modal')
+const modalShow = document.getElementById('show-modal')
+const modalClose = document.getElementById('close-modal')
+const bookmarkForm = document.getElementById('bookmark-form')
+const websiteNameEl = document.getElementById('website-name')
+const websiteUrlEl = document.getElementById('website-url')
+const bookmarksContainer = document.getElementById('bookmarks-container')
+
+// Modal Event Listeners
+modalShow.addEventListener('click', () => {
+   modal.classList.add('show-modal')
+   websiteNameEl.focus()
+})
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'))
+window.addEventListener('click', (e) => {
+   e.target === modal ? modal.classList.remove('show-modal') : false
+})
